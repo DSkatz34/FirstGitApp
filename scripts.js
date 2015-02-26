2504080185fbd20cb9cfc23c6c9df0ce30aacc0c
unknown <dskatz34@gmail.com>
2015-02-25 21:33:42 -0500

    What is this

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 $(function () {
     alert('Hello World');
     alert('Create conflict');
+   
     $('#mtbutton').click(function () {
-        alert  ("What's going on?");
+        alert("What is this?");
+
     });
 });
